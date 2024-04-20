086eefd08d97dfc808f57bfb3a48c26c432003bc
Евгений Кадыков <kadykov_e_v@mail.ru>
2024-04-20 17:41:44 +0300

    Step six, return language link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,6 +64,11 @@
                                 {{ __('app.labels') }}
                             </a>
                         </li>
+                        <li>
+                            <a href="{{ route('locale', [session('locale') ==='ru' ? 'en' : 'ru']) }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                {{ session('locale') ==='ru' ? 'En' : 'Ru' }}
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>
